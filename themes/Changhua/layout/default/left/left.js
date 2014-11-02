edc0edbad52c6f4dcb3777d0ff3adc3a6b57161b
Chiba Lo <littlefoxwenny@gmail.com>
2014-11-03 05:24:10 +0800

    更改選單收縮
    更改選單收縮

--- themes/Changhua/layout/default/left/left.js
+++ themes/Changhua/layout/default/left/left.js
@@ -1,15 +1,2 @@
 
-function toggleMenu( id )
-{
-  var o=document.getElementById( id );
-  o.style.display=
-    (o.style.display=='none') ? 'inline' : 'none';
 
-}
-function bg( id )
-{
-  var dom = document.getElementById(id);
-  dom.style.backgroundImage =
-	(dom.style.backgroundImage=='') ? 'url(../../Changhua/cata/slider_hover.png)' : '';
-}
-hideMenu('menus');
