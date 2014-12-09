578d77b089da7f3f2a8913ca73960f36683d76b5
Chiba Lo <littlefoxwenny@gmail.com>
2014-12-09 16:47:36 +0800

    放送頭修改完成

--- themes/Changhua/blocks/views/backstage/editor2/editor2.js
+++ themes/Changhua/blocks/views/backstage/editor2/editor2.js
@@ -1,12 +1,14 @@
 check = function(){
+	var blog = document.getElementById('blog_name').value;
 	var name = document.getElementById('url_name').value;
 	var url = document.getElementById('url_url').value;
+	url="<a href='"+url+"' target='_blank'>"+name+"</a>";
 	save(name,url);	
 };
 
-function save(name,url) {
+function save(blog,url) {
 	var	pdata={
-	  'title':name,
+	  'title':blog,
 	  'summary':url,
 	  'isPublic':'1'
 	  },
@@ -23,10 +25,13 @@ function save(name,url) {
 };
 
 update = function(ngid){
+	
+	var blog = document.getElementById('blog_name').value;
 	var name = document.getElementById('url_name').value;
 	var url = document.getElementById('url_url').value;
+	url="<a href='"+url+"' target='_blank'>"+name+"</a>";
 	var	pdata={
-	  'title':name,
+	  'title':blog,
 	  'summary':url,
 	  'isPublic':'1'
 	  },
@@ -44,11 +49,16 @@ update = function(ngid){
 
 get_value= function(ngid)
 {
+	
 	var	req = {url: 'changhua/changhua/fun/view/'+ngid, hasCA: true};
 		__.api(req, function(data) {
 		  if (data.errCode === 0) {
-		  document.getElementById('url_name').value = data.value.title;
-		  document.getElementById('url_url').value = data.value.summary;
+		  var str = data.value.summary;
+		  var b = str.search("' target='_blank'>");
+		  var c = str.search("</a>");
+		  document.getElementById('blog_name').value = data.value.title;
+		  document.getElementById('url_name').value = str.substring(b+18,c);
+		  document.getElementById('url_url').value = str.substring(9,b);
 		  } 
 		  else {
 			alert( data.message );
